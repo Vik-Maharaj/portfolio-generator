e0b5ef6127f6f2e1fe11ecd44bf9f5a6fd3cb5ab
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-04-18 20:08:51 -0400

    Update js examples

--- app.js
+++ app.js
@@ -2,25 +2,27 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 console.log(profileDataArgs);
 
-// other examples to follow here
-   
-// Include process module
-const process = require('process');
-  
-// Printing process.argv property value
-console.log(process.argv);
+var message = 'Hello Node!';
 
-// Node.js program to demonstrate the
-// process.argv Property
-   
-// Include process module
-const process = require('process');
-  
-// Printing process.argv property value
-var args = process.argv;
-  
-console.log("number of arguments is "+args.length);
-  
-args.forEach((val, index) => {
-    console.log(`${index}: ${val}`);
-});
+var sun = 5 + 3;
+
+console.log(message);
+
+console.log(sum);
+
+// more examples here
+
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello Node.js World!');
+}).listen(8080);
+
+var http = require('http');
+var dateTime = require('./modules');
+ 
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("Current date and time: " + dateTime.CurrentDateTime());
+    res.end();
+}).listen(8080);

314b3f8a3d08af86c77790fb359b99511728d59d
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-04-20 22:12:44 -0400

    Update js

--- app.js
+++ app.js
@@ -2,40 +2,23 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 console.log(profileDataArgs);
 
-var message = 'Hello Node!';
 
-var sun = 5 + 3;
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i++) {
+        console.log(profileDataArr[i]);
+    }
+    
+};
 
-console.log(message);
-
-console.log(sum);
-
-// more examples here
-
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello Node.js World!');
-}).listen(8080);
-
-var http = require('http');
-var dateTime = require('./modules');
- 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("Current date and time: " + dateTime.CurrentDateTime());
-    res.end();
-}).listen(8080);
+printProfileData(profileDataArgs);
 
 
-var http = require("http");  
-http.createServer(function (request, response) {  
- // Send the HTTP header   
-   // HTTP Status: 200 : OK  
-   // Content Type: text/plain  
-   response.writeHead(200, {'Content-Type': 'text/plain'});  
-   // Send the response body as "Hello World"  
-   response.end('Hello World\n');  
-}).listen(8081);  
-// Console will print the message  
-console.log('Server running at http://127.0.0.1:8081/');  
+
+let two = 'two: declared outside the block';
+
+if (true === true) {
+  let two = 'two: declared inside the block';
+  console.log(two); // prints 'two: declared inside the block'
+}
+
+console.log(two); 
